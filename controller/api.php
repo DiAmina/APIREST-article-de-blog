fefb95e87acf60e6c7a0545baaaba40d39013950
DiAmina <amina.madi97630@gmail.com>
2023-03-26 16:56:41 +0200

    Mise à jour

--- controller/api.php
+++ controller/api.php
@@ -9,61 +9,118 @@ require_once (__DIR__ . "/../model/Article.php");
 require_once (__DIR__ . "/../libs/jwt_utils.php");
 use model\dao\RequestUsers;
 use model\dao\RequestsArticle;
-use model\User;
-use model\Article;
+
 
 
 // Identification du type de méthode HTTP envoyée par le client
 $http_method = $_SERVER['REQUEST_METHOD'];
 $requestArticle = new RequestsArticle();
+$requestUser = new RequestUsers();
+
+
 switch ($http_method) {
     // Cas de la méthode GET
     case "GET":
         $bearer_token = get_bearer_token();
         if (is_jwt_valid($bearer_token)) {
-            // Récupération des critères de recherche envoyés par le Client
-            $matchingData = null;
-            if (isset($_GET['id'])) {
-                $matchingData = $requestArticle->getArticleId($_GET['id']);
-                deliverResponse(200, "Article", $matchingData);
+            $payload = get_jwt_payload($bearer_token);
+            $role = $payload->role;
+            if ($role == 'moderator') {
+                // Récupération des critères de recherche envoyés par le Client
+                $matchingData = null;
+                if (isset($_GET['id'])) {
+                    $matchingData = $requestArticle->getArticleId($_GET['id']);
+                    //revoie les données du client et le nombre de like et dislike
+                    $matchingData['likes'] = $requestArticle->getLikes($_GET['id']);
+                    $matchingData['dislikes'] = $requestArticle->getDislikes($_GET['id']);
+                    deliverResponse(200, "Article", $matchingData);
+                }
+            } else {
+                $matchingData = $requestArticle->getArticles();
+                deliverResponse(200, "Liste des articles", $matchingData);
+            }
+            } else {
+                deliverResponse(401, "Votre token a expiré", null);
+            }
+        break;
+
+        case
+            "POST":
+        $bearer_token = get_bearer_token();
+        if (is_jwt_valid($bearer_token)) {
+            $payload = get_jwt_payload($bearer_token);
+            $role = $payload->role;
+            if ($role == "publisher") {
+                $auteur = $payload->username;
+                $contenu = $_POST['contenu'];
+                $datePub = date("Y-m-d H:i:s");
+                $requestArticle->postArticle($auteur, $contenu, $datePub);
+                deliverResponse(200, "Article mise à jour", null);
+            } else {
+                deliverResponse(401, "Vous n'avez pas le droit d'ajouter un article", null);
             }
         } else {
-            $matchingData = $requestArticle->getArticles();
-            deliverResponse(200, "Liste des articles", $matchingData);
+            deliverResponse(401, "Votre token a expiré", null);
         }
-    case "POST":
         break;
     case "PUT":
-        break;
+        $bearer_token = get_bearer_token();
+        if (is_jwt_valid($bearer_token)) {
+            $payload = get_jwt_payload($bearer_token);
+            $role = $payload->role;
+            if ($role == "publisher") {
+                $auteur = $payload->username;
+                $id = $_GET['id'];
+                $contenu = $_POST['contenu'];
+                $requestArticle->putArticle($id, $contenu);
+                deliverResponse(200, "Article modifié", null);
+            } else {
+                deliverResponse(401, "Vous n'avez pas le droit de modifier un article", null);
+            }
+        } else {
+            deliverResponse(401, "Votre token a expiré", null);
+        }
+    break;
+
     case "DELETE":
+        $bearer_token = get_bearer_token();
+        if (is_jwt_valid($bearer_token)) {
+            $payload = get_jwt_payload($bearer_token);
+            $role = $payload->role;
+            if ($role == "publisher") {
+                $id = $_GET['id'];
+                $requestArticle->deleteLike($id);
+                $requestArticle->deletedislike($id);
+                $requestArticle->deleteArticle($id);
+                deliverResponse(200, "Article supprimé", null);
+            } else {
+                deliverResponse(401, "Vous n'avez pas le droit de supprimer un article", null);
+            }
+        } else {
+            deliverResponse(401, "Votre token a expiré", null);
+        }
         break;
     default:
         deliverResponse(405, "Méthode non autorisée", null);
         break;
 }
 // Envoi de la réponse au Client
-function deliverResponse($status, $statusMessage, $data): void
-{
-    // Paramétrage de l'entête HTTP, suite
-    header("HTTP/1.1 $status $statusMessage");
+        function deliverResponse($status, $statusMessage, $data): void
+        {
+            // Paramétrage de l'entête HTTP, suite
+            header("HTTP/1.1 $status $statusMessage");
 
-    // Paramétrage de la réponse retournée
-    $response['status'] = $status;
-    $response['status_message'] = $statusMessage;
-    $response['data'] = $data;
+            // Paramétrage de la réponse retournée
+            $response['status'] = $status;
+            $response['status_message'] = $statusMessage;
+            $response['data'] = $data;
 
-    // Mapping de la réponse au format JSON
-    $jsonResponse = json_encode($response);
-    echo $jsonResponse;
+            // Mapping de la réponse au format JSON
+            $jsonResponse = json_encode($response);
+            echo $jsonResponse;
+
+
+
 }
-
-
-/* algo
-si le rôle est publisher then
-    post, get
-    fin if;
-    if le nom de l'auteur == a nom du publisher connecter the
-        delete, post, put
-    fin if;
-*/
+?>
 
